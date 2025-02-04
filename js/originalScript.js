e365c88b5bf020f4f15a0732b455111a88d2f853
Sample-Calendar <sayath.ji+calendar@gmail.com>
2025-02-04 11:12:00 -0600

    Committing Custom fields

--- js/originalScript.js
+++ js/originalScript.js
@@ -3,7 +3,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     const calendarEl = document.getElementById('calendar')
     const calendar = new FullCalendar.Calendar(calendarEl, {
-      initialView: 'dayGridMonth',initialDate: '2025-01-07',
+      initialView: 'dayGridMonth',
+      initialDate: '2025-01-07',
       headerToolbar: {
         left: 'prev,next today',
         center: 'title',
@@ -13,73 +14,98 @@ document.addEventListener('DOMContentLoaded', function() {
         {
           title: 'All Day Event',
           start: '2025-01-01',
-          description: 'Travelling to Hyd',
-          location: 'JFK'
+          person: '',
+          phone: '',
+          origin: '',
+          destination: ''
         },
         {
           title: 'Long Event',
           start: '2025-01-07',
           end: '2025-01-10',
-          description: 'Travelling to Hyd',
-          location: 'JFK'
+          person: '',
+          phone: '',
+          origin: '',
+          destination: ''
         },
         {
           groupId: '999',
           title: 'Repeating Event',
           start: '2025-01-09T16:00:00',
-          description: 'Travelling to Hyd',
-          location: 'JFK'
+          person: '',
+          phone: '',
+          origin: '',
+          destination: ''
         },
         {
           groupId: '999',
           title: 'Repeating Event',
           start: '2025-01-16T16:00:00',
-          description: 'Travelling to Hyd',
-          location: 'JFK'
+          person: '',
+          phone: '',
+          origin: '',
+          destination: ''
         },
         {
           title: 'Conference',
           start: '2025-01-11',
           end: '2025-01-13',
-          description: 'Travelling to Hyd',
-          location: 'JFK'
+          person: '',
+          phone: '',
+          origin: '',
+          destination: ''
         },
         {
           title: 'Meeting',
           start: '2025-01-12T10:30:00',
           end: '2025-01-12T12:30:00',
-          description: 'Travelling to Hyd',
-          location: 'JFK'
+          person: '',
+          phone: '',
+          origin: '',
+          destination: ''
         },
         {
           title: 'Lunch',
           start: '2025-01-12T12:00:00',
-          description: 'Travelling to Hyd',
-          location: 'JFK'
+          person: '',
+          phone: '',
+          origin: '',
+          destination: ''
         },
         {
           title: 'Meeting',
           start: '2025-01-12T14:30:00',
-          description: 'Travelling to Hyd',
-          location: 'JFK'
+          person: '',
+          phone: '',
+          origin: '',
+          destination: ''
         },
         {
           title: 'Birthday Party',
           start: '2025-01-13T07:00:00',
-          description: 'Travelling to Hyd',
-          location: 'JFK'
+          person: '',
+          phone: '',
+          origin: '',
+          destination: ''
         },
         {
           title: 'Click for Google',
-          url: 'https://google.com/',
           start: '2025-01-28',
-          description: 'Travelling to Hyd',
-          location: 'JFK'
+          person: '',
+          phone: '',
+          origin: '',
+          destination: ''
         }
       ],
       eventContent: function(arg) { 
         return { 
-          html: `${arg.event.title} - ${arg.event.extendedProps.description} (${arg.event.extendedProps.location})` 
+          html: `
+          ${arg.event.title} <br>
+          Person: ${arg.event.extendedProps.person} <br>
+          Phone: ${arg.event.extendedProps.phone} <br>
+          Origin: ${arg.event.extendedProps.origin} <br>
+          Destination: ${arg.event.extendedProps.destination}
+        ` 
         };
       }
     })
