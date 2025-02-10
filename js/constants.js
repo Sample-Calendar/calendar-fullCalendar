520507cae69e6795b8f6262fcedcb1b684a9d889
Sample-Calendar <sayath.ji+calendar@gmail.com>
2025-02-09 20:57:27 -0600

    Multiple Changes
    Css Sticker, Filters moved to seperate JS, updates to HTML

---
+++ js/constants.js
@@ -0,0 +1,8 @@
+const ports = [
+  { value: 'Any', label: 'All' },
+  { value: 'New York', label: 'New York' },
+  { value: 'Los Angeles', label: 'Los Angeles' },
+  { value: 'London', label: 'London' }
+];
+
+export default ports;
--- js/originalScript.js
+++ js/originalScript.js
@@ -1,9 +1,107 @@
 // js/originalscript.js
 import { events } from './eventData.js';
+import { ports } from './constants.js';
 
 document.addEventListener('DOMContentLoaded', function() {
+    /*const events =  [
+      {
+        title: 'Looking for a Companion',
+        start: '2025-01-01',
+        person: 'Ram',
+        phone: '',
+        origin: 'MSP',
+        type: 'traveler',
+        destination: 'HYD'
+      },
+      {
+        title: 'Help my mom',
+        start: '2025-01-07',
+        end: '2025-01-10',
+        person: 'Laxman',
+        phone: '',
+        origin: 'JFK',
+        type: 'traveler',
+        destination: 'HYD'
+      },
+      {
+        groupId: '999',
+        title: 'Help me',
+        start: '2025-01-09T16:00:00',
+        person: 'Bharath',
+        phone: '',
+        origin: 'DLS',
+        type: 'traveler',
+        destination: 'DLH'
+      },
+      {
+        title: 'Travel Companion for Mom',
+        start: '2025-01-16T16:00:00',
+        end: '2025-01-16T18:00:00',
+        person: 'Sita',
+        phone: '',
+        origin: 'AST',
+        type: 'traveler',
+        destination: 'TPT'
+      },
+      {
+        title: 'Travel Companion for Mom',
+        start: '2025-02-11',
+        end: '2025-02-13',
+        person: 'Urmila',
+        phone: '',
+        origin: 'NWL',
+        type: 'traveler',
+        destination: 'VZG'
+      },
+      {
+        title: 'Ready to Help',
+        start: '2025-01-12',
+        end: '2025-01-12',
+        person: 'Karna',
+        phone: '',
+        origin: 'PSU',
+        type: 'helper',
+        destination: 'HYD'
+      },
+      {
+        title: 'Travel Companion for Mom',
+        start: '2025-01-12',
+        person: 'Arjuna',
+        phone: '',
+        origin: 'WSL',
+        type: 'traveler',
+        destination: 'PUN'
+      },
+      {
+        title: 'Travel Companion for Mom',
+        start: '2025-01-12T14:30:00',
+        end: '2025-01-13T13:30:00',
+        person: 'Bheema',
+        phone: '',
+        origin: 'PST',
+        type: 'traveler',
+        destination: 'DLH'
+      },
+      {
+        title: 'Travel Companion for Mom',
+        start: '2025-01-13T07:00:00',
+        person: 'Nakul',
+        phone: '',
+        origin: 'CST',
+        type: 'traveler',
+        destination: 'HYD'
+      },
+      {
+        title: 'Willing to help',
+        start: '2025-01-28',
+        person: 'Sahayam',
+        phone: '',
+        origin: 'EST',
+        type: 'helper',
+        destination: 'BLR'
+      }
+    ];*/
     const events = events;
-    console.log('Imported events '+importedEvents);
     const calendarEl = document.getElementById('calendar');
     const originFilter = document.getElementById('origin-filter'); 
     const destinationFilter = document.getElementById('destination-filter');
@@ -41,6 +139,7 @@ document.addEventListener('DOMContentLoaded', function() {
         };
       }
     });
+    populateSelectOptions();
     calendar.render();
 
 
@@ -77,3 +176,15 @@ document.addEventListener('DOMContentLoaded', function() {
     calendar.setOption('events', filteredEvents);
   });
 });
+
+function populateSelectOptions() {
+  filters.forEach(filter => {
+    const selectElement = document.getElementById(`${filter.name}-filter`);
+    filter.options.forEach(option => {
+      const optionElement = document.createElement('option');
+      optionElement.value = option.value;
+      optionElement.textContent = option.label;
+      selectElement.appendChild(optionElement);
+    });
+  });
+}
